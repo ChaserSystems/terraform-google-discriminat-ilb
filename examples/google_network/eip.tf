349bc0bbf41aeaf71b9572d653bf5b2160256509
new23d <4802032+new23d@users.noreply.github.com>
2025-08-22 15:40:42 +0100

    spare ip in example

--- examples/google_network/eip.tf
+++ examples/google_network/eip.tf
@@ -37,3 +37,23 @@ resource "google_compute_address" "nat_b" {
     prevent_destroy = false
   }
 }
+
+resource "google_compute_address" "nat_c" {
+  name = "static-egress-ip-c"
+
+  address_type = "EXTERNAL"
+
+  region  = module.google_network.subnets["europe-west2/subnet-foo"].region
+  project = var.project_id
+
+  labels = {
+    # set the value of label 'discriminat' to custom_deployment_id as passed to
+    # the discriminat module if pinning this External IP to that particular
+    # fleet of DiscrimiNAT's is desired
+    "discriminat" = "whatsoever_or_custom-deployment-id"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
